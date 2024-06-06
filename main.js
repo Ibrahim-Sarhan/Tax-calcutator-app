4e049b86b28bc6ea5572ba42973b51d00c88a543
Ibrahim-Sarhan <samamedym33@gmail.com>
2024-06-06 11:08:18 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+// Tax Calculator app
+
+let billValue = +prompt("Enter the bill value please : ");
+
+let taxValue = billValue * 0.14;
+
+let totalValue = "The total value , you will pay after add 14% : " + (taxValue + billValue);
+
+console.log(totalValue);
+
+document.write(totalValue);
